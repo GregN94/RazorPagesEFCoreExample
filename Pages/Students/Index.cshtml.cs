c9f1cca920a0a7b64d5ea7809a0938f61795ed64
GregN94 <grzesiekn1001@gmail.com>
2020-09-22 22:33:17 +0200

    adding CRUD students support

---
+++ Pages/Students/Index.cshtml.cs
@@ -0,0 +1,29 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using ContosoUniveristy.Models;
+using ContosoUniversity.Data;
+
+namespace ContosoUniveristy.Pages.Students
+{
+    public class IndexModel : PageModel
+    {
+        private readonly SchoolContext _context;
+
+        public IndexModel(SchoolContext context)
+        {
+            _context = context;
+        }
+
+        public IList<Student> Student { get; set; }
+
+        public async Task OnGetAsync()
+        {
+            Student = await _context.Students.ToListAsync();
+        }
+    }
+}
